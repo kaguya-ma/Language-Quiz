d0b0a5fba65272b1682257a4e1dc49e094576e4a
Ma Tester <lulamae1983@gmail.com>
2020-10-20 21:30:46 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function startPage() {
+  document.querySelector("#sentence").style.display="none"
+  document.querySelector("#word").style.display="none"
+  document.querySelector("#button1").style.display="none"
+  document.querySelector("#button2").style.display="none"
+  document.querySelector("#button3").style.display="none"
+}
+
+
+function startQuiz() {
+  document.querySelector("#sentence").style.display="inline"
+  document.querySelector("#buttonStart").style.display="inline"
+  document.querySelector("#button1").style.display="inline"
+  document.querySelector("#button2").style.display="inline"
+  document.querySelector("#button3").style.display="inline"
+}
